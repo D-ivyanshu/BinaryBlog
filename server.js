405b38b09ca0e975a550309a80cc4d2584b8e4ae
D-ivyanshu <93874215+D-ivyanshu@users.noreply.github.com>
2023-03-08 23:11:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
+const MongoStore = require('connect-mongo')(session);
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -48,7 +49,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 
+const store = new MongoStore({
+    url : dbUrl,
+    secret: 'thisshouldbeabettersecret!',
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error",  function(e){
+    console.log("Error", e)
+})
+
 const sessionConfig = {
+    store,
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
